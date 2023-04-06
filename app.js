e56ee8b60388a9da5a22b41dbce2c938ab9ade08
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-04-06 10:21:56 +0530

    user model and route added

--- app.js
+++ app.js
@@ -2,21 +2,33 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const userRoutes = require('./api/routes/Users');
 
 const app = express();
-mongoose.connect("mongodb+srv://sanjaynithin:" + process.env.MONGO_PASSWORD + "@cluster0.kgz6ota.mongodb.net/?retryWrites=true&w=majority");
 app.use(morgan('dev'));
 app.use('/uploads',express.static('uploads'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
+// handling CORS
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Accept, Authorization, Content-Type");
+    if(req.method === 'OPTIONS'){
+        res.header(
+            "Access-Control-Allow-Methods", 
+            "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 
+mongoose.connect("mongodb+srv://sanjaynithin2002:" +process.env.MONGODB_PASSWORD +  "@cluster0.kgz6ota.mongodb.net/?retryWrites=true&w=majority");
 
-
-
-
-
+app.use('/users', userRoutes);
 app.use((req,res,next)=>{
     const error = new Error("Not Found");
     error.status = 404;
